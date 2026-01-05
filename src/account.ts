809e13ec1d90eb36472738d043617b4be9699476
Baptiste Pillot <baptiste@pillot.fr>
2026-01-05 14:09:37 +0100

    Coding standards

--- src/account.ts
+++ src/account.ts
@@ -3,7 +3,8 @@ import { Password }       from '@itrocks/password'
 import { Required }       from '@itrocks/required'
 import { Store }          from '@itrocks/store'
 
-@Representative('login') @Store()
+@Representative('login')
+@Store()
 export class Account
 {
 
